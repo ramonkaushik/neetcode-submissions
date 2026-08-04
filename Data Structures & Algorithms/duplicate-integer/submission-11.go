ef5e3e71459bc23f0451a5491b93bd8a328e67aa
ramonkaushik <43421387+ramonkaushik@users.noreply.github.com>
2026-08-04 10:58:34 -0400

    Add: duplicate-integer - submission-11

---
+++ Data Structures & Algorithms/duplicate-integer/submission-11.go
@@ -0,0 +1,13 @@
+func hasDuplicate(nums []int) bool {
+    dupes := make(map[int]bool)
+
+    for _, num := range(nums) {
+        _, exists := dupes[num]
+
+        if exists {
+            return true
+        }
+        dupes[num] = true
+    }
+    return false
+}
